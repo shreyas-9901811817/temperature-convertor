35e1c41c20259b6256772c273c62a1310d475cf4
shreyas-9901811817 <149670827+shreyas-9901811817@users.noreply.github.com>
2023-12-04 22:06:30 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,50 @@
+let celciusInput=document.querySelector('#celcius>input')
+let FaherinetInput=document.querySelector('#Faherinet>input')
+let kelvinInput=document.querySelector('#kelvin>input')
+
+let btn=document.querySelector('.button button')
+
+function roundNumber(number)
+{
+    return Math.round(number*100)/100
+}
+celciusInput.addEventListener('input',function()
+{
+let cTemp=parseFloat(celciusInput.value)
+let fTemp=(cTemp*(9/5))+32
+let kTemp=cTemp+273.15
+FaherinetInput.value=roundNumber(fTemp)
+kelvinInput.value=roundNumber(kTemp)
+})
+
+
+
+FaherinetInput.addEventListener('input',function()
+{
+let fTemp=parseFloat(FaherinetInput.value)
+let cTemp=(fTemp-32)*(5/9)
+let kTemp=(fTemp-32)*(5/9)+273.15
+celciusInput.value=roundNumber(cTemp)
+kelvinInput.value=roundNumber(kTemp)
+})
+
+
+kelvinInput.addEventListener('input',function()
+{
+let kTemp=parseFloat(kelvinInput.value)
+let cTemp=(kTemp-273.15)
+let fTemp=(kTemp-273.15)*(9/5)+32
+celciusInput.value=roundNumber(cTemp)
+FaherinetInput.value=roundNumber(fTemp)
+})
+
+btn.addEventListener('click',()=>{
+celciusInput.value=""
+FaherinetInput.value=""
+kelvinInput.value=""
+})
+
+
+
+
+
